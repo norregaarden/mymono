9fb79c800c76d546cf4c1c0de470fe18cbd8c0f0
Johan Nørregaard Andersen <jna.jelling@gmail.com>
2023-08-07 02:46:52 +0200

    cleanup components/Input.tsx

--- web/lol/src/components/Input.tsx
+++ web/lol/src/components/Input.tsx
@@ -1,34 +1,7 @@
 import {
   JSX,
-  Setter,
 } from "solid-js";
-import { UnionPick } from "../../../../todd/uti"
-import { TypeStringMap } from "../../../../todd/scheme"
-import { GetSet, GetSetUnidist } from "../../../../soft/ge";
-
-export function InputLine<
-  T extends UnionPick<keyof TypeStringMap, 'string' | 'number'>>(props: {
-    type: T; signal: GetSetUnidist<TypeStringMap[typeof props.type & string]>;
-  }) {
-  // const onChangeSetter: JSX.EventHandler<HTMLInputElement, InputEvent> = (ev) =>
-  // const onChangeSetter: JSX.InputEventHandlerUnion<HTMLInputElement, InputEvent> = (ev) =>
-  const onChangeSetter: JSX.ChangeEventHandlerUnion<HTMLInputElement, Event> = (ev) =>
-    props.type == 'number'
-      ? (props.signal.set as Setter<number>)(parseFloat(ev.target.value))
-      : (props.signal.set as Setter<string>)(ev.target.value)
-
-  return (
-    <div>
-      <div>
-        <input
-          type={props.type}
-          value={props.signal.get()}
-          onChange={onChangeSetter}
-        />
-      </div>
-    </div>
-  );
-}
+import { GetSet, } from "../../../../soft/ge";
 
 type InputProps<T> = {
   getset: GetSet<T>,
@@ -50,28 +23,10 @@ export function InputNumber(props: InputProps<number>) {
 
 export function InputString(props: InputProps<string>) {
   const eventSetter: JSX.ChangeEventHandler<HTMLInputElement, Event> = (ev) => {
-    // const maybeNumber = parseFloat(ev.target.value)
-    // if (!isNaN(maybeNumber) && isFinite(maybeNumber)) {
     props.getset.set(ev.target.value)
-    // }
   }
   return <div>
     <input type="" value={props.getset.get()} onChange={eventSetter} />
   </div>
 }
-
-// export function InputArea(props: {
-//   signal: GetSet<TypeStringMap["text"]>;
-// }) {
-//   return (
-//     <div>
-//       <div>
-//         <textarea
-//           value={props.signal.get()}
-//           onChange={(ev) => props.signal.set(ev.target.value)}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
 
